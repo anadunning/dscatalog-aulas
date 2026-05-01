310452d9d517bd3b9a74975e48eedaf8473041e7
anadunning <anakpedrosa@hotmail.com>
2026-04-30 22:44:20 -0300

    H2 Database

---
+++ backend/src/main/java/com/anadunning/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.anadunning.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.anadunning.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
